2cf741c937dd0834d2622a046ed4c7af357c9295
Darturium <darturium@gmail.com>
2014-03-14 11:15:34 -0400

    Added classes Card, Deck, Player, and Dealer

---
+++ blackjack.rb
@@ -0,0 +1,47 @@
+class Card
+  attr_accessor :suit, :value
+
+  def initialize(s, v)
+    @suit = s
+    @value = v
+  end
+
+  def to_s
+    "This is the card! #{suit}, #{value}"
+  end
+end
+
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    ['H', 'D', 'S', 'C'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
+    end
+    @cards = @cards * num_decks
+    scramble!
+  end
+
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal
+    cards.pop
+  end 
+  
+end
+
+class Player
+
+end
+
+class Dealer
+
+end
+
+
+ 
